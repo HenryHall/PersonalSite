42c5187c9da8129a726d58571449ef610f31b583
HenryHall <mnboogie@yahoo.com>
2018-12-15 04:38:48 -0600

    Logo Draw

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,61 +1,137 @@
 
 
 var scene = new THREE.Scene();
-var canvas = document.getElementById("myCanvas");
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
-var camera = new THREE.PerspectiveCamera( 75, canvas.width / canvas.height, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
-renderer = new THREE.WebGLRenderer({ canvas: canvas });
+var sCanvas = document.getElementById("myCanvas");
+sCanvas.height = window.innerHeight;
+sCanvas.width = window.innerWidth;
+var camera = new THREE.PerspectiveCamera( 40, sCanvas.width / sCanvas.height, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer = new THREE.WebGLRenderer({ canvas: sCanvas });
+
+var raycaster = new THREE.Raycaster();
+var mouseVect = new THREE.Vector2();
 
 
-window.addEventListener("resize", function(){
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
-  renderer.setSize( canvas.width, canvas.height );
-  camera.aspect = canvas.width / canvas.height;
+window.addEventListener("resize", onWindowResize);
+sCanvas.addEventListener("mousemove", onMouseMove);
+
+function onWindowResize(){
+  sCanvas.height = window.innerHeight;
+  sCanvas.width = window.innerWidth;
+  renderer.setSize( sCanvas.width, sCanvas.height );
+  camera.aspect = sCanvas.width / sCanvas.height;
   camera.updateProjectionMatrix();
-});
+}
+
+function onMouseMove(e){
+  e.preventDefault();
+
+  var canvasRect = sCanvas.getBoundingClientRect();
+  var vector = new THREE.Vector2( (e.clientX - canvasRect.left) / canvasRect.width, (e.clientY - canvasRect.top) / canvasRect.height );
+  mouseVect.set( ( vector.x * 2 ) - 1, - ( vector.y * 2 ) + 1 );
+  raycaster.setFromCamera( mouseVect, camera );
+
+  //Array of objects the mouse is over, nearest to farthest
+  var intersects = raycaster.intersectObjects( scene.children );
+
+  if( intersects.length > 0 && intersects[0].uv ){
+    switch (intersects[0].object) {
+      case bizCard:
+        console.log("Mouse over bizCard!");
+        break;
+      default:
+        console.log("No mouse over");
+    }
+  }
+}
 
 
 var cardFrontCnvs = document.createElement('canvas');
 var cfCtx = cardFrontCnvs.getContext('2d');
-cfCtx.fillStyle = "blue";
-cfCtx.fillRect(0, 0, cardFrontCnvs.width, cardFrontCnvs.height);
+var frontTexture = new THREE.CanvasTexture( cardFrontCnvs );
+cardFrontCnvs.height = 256;
+cardFrontCnvs.width = 512;
+frontTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
+
 
 var cardBackCnvs = document.createElement('canvas');
 var cbCtx = cardBackCnvs.getContext('2d');
-cbCtx.fillStyle = "red";
-cbCtx.fillRect(0, 0, cardBackCnvs.width, cardBackCnvs.height);
+var backTexture = new THREE.CanvasTexture( cardBackCnvs );
+cardBackCnvs.height = 256;
+cardBackCnvs.width = 512;
+backTexture.anisotropy = renderer.capabilities.getMaxAnisotropy();
 
-var geometry = new THREE.BoxGeometry( 8, 4, .05 );
 
-// loader.setPath( '../assets/' );
-// var textureCube = loader.load(['papertexture.png', 'papertexture.png', 'papertexture.png', 'papertexture.png', 'papertexture.png', 'papertexture.png']);
+var geometry = new THREE.BoxGeometry( 8, 4, .03 );
+
+geometry.computeFaceNormals();
+
 
 var materialArray = [
-  new THREE.MeshBasicMaterial({color: 0x0000ff}),   //Left
-  new THREE.MeshBasicMaterial({color: 0xffffff}),   //Right
-  new THREE.MeshBasicMaterial({color: 0xffffff}),   //Top
-  new THREE.MeshBasicMaterial({color: 0xffffff}),   //Bottom
-  // new THREE.MeshBasicMaterial({color: 0x0000ff}),   //Front
-  // new THREE.MeshBasicMaterial({color: 0xff0000}),   //Back
-  new THREE.MeshBasicMaterial({map: new THREE.CanvasTexture( cardFrontCnvs )}),     //Front
-  new THREE.MeshBasicMaterial({map: new THREE.CanvasTexture( cardBackCnvs )})     //Back
+  new THREE.MeshBasicMaterial({ color: 0xffffff }),   //Left
+  new THREE.MeshBasicMaterial({ color: 0xffffff }),   //Right
+  new THREE.MeshBasicMaterial({ color: 0xffffff }),   //Top
+  new THREE.MeshBasicMaterial({ color: 0xffffff }),   //Bottom
+  new THREE.MeshBasicMaterial({ map: frontTexture }),     //Front
+  new THREE.MeshBasicMaterial({ map: backTexture })     //Back
 ];
 
 var bizCard = new THREE.Mesh( geometry, new THREE.MeshFaceMaterial(materialArray) );
 
 
-//Check this: http://bl.ocks.org/MAKIO135/eab7b74e85ed2be48eeb
+function drawCardFront(){
 
+  //Card Texture
+  cfCtx.fillStyle = '#eeeeee';
+  cfCtx.fillRect(0, 0, cardBackCnvs.width, cardBackCnvs.height);
 
-function updateCardFront(){
-  cfCtx.fillText("Henry Hall", canvas.height, canvas.width);
+  //Card Text
+  cfCtx.font = '50px serif';
+  cfCtx.textAlign = 'center';
+  cfCtx.fillStyle = "black";
+  cfCtx.fillText("Front Side", cardFrontCnvs.width / 2, cardFrontCnvs.height / 2);
+
 }
 
 
-function updateCardBack(){
+function drawCardBack(){
+
+  //Texture
+  cbCtx.fillStyle = '#eeeeee';
+  cbCtx.fillRect(0, 0, cardBackCnvs.width, cardBackCnvs.height);
+
+  //Text
+  // cbCtx.font = '50px serif';
+  // cbCtx.textAlign = 'center';
+  // cbCtx.fillStyle = "black";
+  // cbCtx.fillText("Back Side", cardBackCnvs.width / 2, cardBackCnvs.height / 2);
+
+  //Animation
+  var logoSrcs = [
+    '../assets/github.png',
+    '../assets/linkedin.png',
+    '../assets/codepen.png'
+  ];
+
+  var logoSquare, shift, dx, dy;
+  if( cardBackCnvs.width > cardBackCnvs.height ){
+    logoSquare = cardBackCnvs.width * ( 1 / ( logoSrcs.length + 1 ) );
+    shift = logoSquare * .25;
+    dx = shift;
+    dy = (cardBackCnvs.height / 2) - (logoSquare / 2);
+  } else {
+    logoSquare = cardBackRadius.height * ( 1 / ( logoSrcs.length + 1 ) );
+    shift = logoSquare * .25;
+    dx = (cardBackCnvs.width / 2) -  (logoSquare / 2);
+    dy = shift;
+  }
+
+  logoSrcs.forEach( (logoSrc) => {
+    let img = new Image();
+    img.src = logoSrc;
+    cbCtx.drawImage( img, dx, dy, logoSquare, logoSquare );
+    dx = dx + logoSquare + shift;
+  });
 
 }
 
@@ -63,15 +139,20 @@ function updateCardBack(){
 scene.add( bizCard );
 camera.position.z = 10;
 
+
 function animate() {
 	requestAnimationFrame( animate );
+  cfCtx.clearRect(0, 0, cardFrontCnvs.width, cardFrontCnvs.height);
+  cbCtx.clearRect(0, 0, cardFrontCnvs.width, cardFrontCnvs.height);
 
-  updateCardFront();
-  // updateCardBack();
-  // textureCube.needsUpdate = true;
+  drawCardFront();
+  drawCardBack();
 
-  bizCard.rotation.x += .01;
-  // bizCard.rotation.y -= .01;
+  frontTexture.needsUpdate = true;
+  backTexture.needsUpdate = true;
+
+  // bizCard.rotation.x += .01;
+  bizCard.rotation.y -= .005;
 
 	renderer.render( scene, camera );
 }
